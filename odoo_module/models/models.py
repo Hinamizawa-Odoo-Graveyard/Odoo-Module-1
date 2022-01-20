053eee36600a09f709baf24e9d9dd794c809cf6b
Hinamizawa <themoonbunnys@gmail.com>
2022-01-20 10:02:14 -0400

    update

--- odoo_module/models/models.py
+++ odoo_module/models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class odoo_module(models.Model):
-#     _name = 'odoo_module.odoo_module'
-#     _description = 'odoo_module.odoo_module'
+class odoo_module(models.Model):
+     _name = 'odoo_module.odoo_module'
+     _description = 'odoo_module.odoo_module'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
+
+     @api.depends('value')
+     def _value_pc(self):
+         for record in self:
+             record.value2 = float(record.value) / 100
