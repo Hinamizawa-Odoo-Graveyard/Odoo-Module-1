6fbca9dcd9aa60bbceac51c5274352a225b1f0ec
Hinamizawa <themoonbunnys@gmail.com>
2022-01-20 09:49:44 -0400

    Remove comments
    :3

--- odoo_module/controllers/controllers.py
+++ odoo_module/controllers/controllers.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class OdooModule(http.Controller):
-#     @http.route('/odoo_module/odoo_module/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class OdooModule(http.Controller):
+     @http.route('/odoo_module/odoo_module/', auth='public')
+     def index(self, **kw):
+         return "Hello, world"
 
-#     @http.route('/odoo_module/odoo_module/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('odoo_module.listing', {
-#             'root': '/odoo_module/odoo_module',
-#             'objects': http.request.env['odoo_module.odoo_module'].search([]),
-#         })
+     @http.route('/odoo_module/odoo_module/objects/', auth='public')
+     def list(self, **kw):
+         return http.request.render('odoo_module.listing', {
+             'root': '/odoo_module/odoo_module',
+             'objects': http.request.env['odoo_module.odoo_module'].search([]),
+         })
 
-#     @http.route('/odoo_module/odoo_module/objects/<model("odoo_module.odoo_module"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('odoo_module.object', {
-#             'object': obj
-#         })
+     @http.route('/odoo_module/odoo_module/objects/<model("odoo_module.odoo_module"):obj>/', auth='public')
+     def object(self, obj, **kw):
+         return http.request.render('odoo_module.object', {
+             'object': obj
+         })
